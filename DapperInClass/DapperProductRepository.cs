4bffbebadc1f6f32104b3983d49b3afc66b63f3d
David Smith <cpmpdavidsmith@icloud.com>
2023-03-10 10:07:32 -0500

    initial save

--- DapperInClass/DapperProductRepository.cs
+++ DapperInClass/DapperProductRepository.cs
@@ -7,16 +7,43 @@ namespace DapperInClass
 {
 	public class DapperProductRepository : IProductRepository
 	{
-        private readonly IDbConnection _connection; //field called _connection of type IDbConnection 
-		public DapperProductRepository(IDbConnection connection)            //the private message is an example oof "encapsulation" where every time an instance is made of line 7, a connection(with password!!) called 'IDbConnection' is run in 'constructor' line 11 and stored in field (which is unaccessable by the world) and is 'private" line 10
+        private readonly IDbConnection _connection;                                                         //field called _connection of type IDbConnection 
+		public DapperProductRepository(IDbConnection connection)                                            //the private message is an example oof "encapsulation" where every time an instance is made of line 7, a connection(with password!!) called 'IDbConnection' is run in 'constructor' line 11 and stored in field (which is unaccessable by the world) and is 'private" line 10
 		{
             _connection = connection;
 		}
 
-        IEnumerable<Product> IProductRepository.GetAllProducts()
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            _connection.Execute("INSERT INTO products (Name, Price, CategoryID)" +
+                "VALUES (@name, @price, @categoryID);"
+                , new { name = name, price = price, categoryID = categoryID });
+
+        }
+
+        public IEnumerable<Product> GetAllProducts()
         {
             return _connection.Query<Product>("SELECT * FROM products");
         }
+
+        public void UpdateProductName(int  productID, string updatedName)
+        {
+            _connection.Execute("UPDATE products SET Name = @updatedName WHERE ProductID = @productID;",   //update data
+                new { updatedName = updatedName, productID = productID });
+        }
+
+        public void DeleteProduct(int productID)
+        {
+            _connection.Execute("DELETE FROM reviews WHERE ProductID = @productID;",
+                new { productID = productID });
+
+            _connection.Execute("DELETS FROM sale WHERE ProductID = @productID;",
+                new { productID = productID });
+
+            _connection.Execute("DELETE FROM products WHERE ProductID = @productID;",
+                new { productID = productID });
+        }
+
     }
 }
 
--- DapperInClass/IProductRepository.cs
+++ DapperInClass/IProductRepository.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace DapperInClass
 {
 	public interface IProductRepository
 	{
-		IEnumerable<Product> GetAllProducts();
+		public IEnumerable<Product> GetAllProducts();
 	}
 }
 
